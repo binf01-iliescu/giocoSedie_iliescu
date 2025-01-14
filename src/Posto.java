0b8210cb59c93cd025179cbbb94293a8f0d4b3e2
Gab2006 <tostigabriele6@gmail.com>
2025-01-14 12:36:00 +0100

    Primo commit

---
+++ src/Posto.java
@@ -0,0 +1,23 @@
+
+class Posto
+
+{
+	private boolean occupato;
+
+	public Posto()
+
+	{
+		occupato = false;
+	}
+
+	public synchronized boolean libero() {
+		return (!occupato);
+	}
+
+	public synchronized boolean occupa() {
+		if (occupato)
+			return false;
+		else
+			return occupato = true;
+	}
+}
